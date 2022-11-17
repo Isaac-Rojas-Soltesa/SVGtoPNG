6f94e2a732eb217a122878177b21d22379da6e3b
Isaac-Rojas-Soltesa <67087864+Isaac-Rojas-Soltesa@users.noreply.github.com>
2022-11-17 17:29:09 -0600

    uuid

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 var fs = require("fs");
 var pdf = require("pdf-creator-node");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 
 app.post('/convert', async(req, res) => {
     
-    var id = uuid.v4();
+    var id = uuidv4();
     var html = req.body.html;
     var path = './file/' + id.trim() + '.pdf';
     var document = {
